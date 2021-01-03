19c09f79b7ed5de89241ed6d81ca8e8db73a70d2
heikki siikavirta <heikki.siikavirta@kidesystems.com>
2021-01-03 14:41:33 +0200

    proceeded Java

---
+++ Section 03/tutorial_29_float_double_primitive_types/Main.java
@@ -0,0 +1,23 @@
+package tutorial_29_float_double_primitive_types;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		float myMinFloatValue = Float.MIN_VALUE;
+		float myMaxFloatValue = Float.MAX_VALUE;
+		System.out.println("Float minimum value = " + myMinFloatValue);
+		System.out.println("Float maximum value = " + myMaxFloatValue);
+
+		double myMinDoubleValue = Double.MIN_VALUE;
+		double myMaxDoubleValue = Double.MAX_VALUE;
+		System.out.println("Double minimum value = " + myMinDoubleValue);
+		System.out.println("Double maximum value = " + myMaxDoubleValue);
+
+		int myIntValue = 5;
+		float myFloatValue = 5.25f;
+//		float myFloatValue = 5.25; error double is implicitly assumed
+		double myDoubleValue = 5.25d; // Double is recomended over float
+
+	}
+}

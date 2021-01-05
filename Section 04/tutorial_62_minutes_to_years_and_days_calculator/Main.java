6ada156967942c0e70ee1fe2e31bcfadbb3cd385
Heikki Siikavirta <heikki.siikavirta@gmail.com>
2021-01-05 18:48:54 +0200

    finished minutes challange

---
+++ Section 04/tutorial_62_minutes_to_years_and_days_calculator/Main.java
@@ -0,0 +1,25 @@
+package tutorial_62_minutes_to_years_and_days_calculator;
+
+
+public class Main {
+/*
+
+ Use the remainder operator
+
+ 1 hour = 60 minutes
+
+ 1 day = 24 hours
+
+ 1 year = 365 days
+
+ 
+ */
+  public static void main(String[] args) {
+	  
+	  MinutesToYearsDaysCalculator.printYearsAndDays(525600);  
+	  MinutesToYearsDaysCalculator.printYearsAndDays(1051200); 
+	  MinutesToYearsDaysCalculator.printYearsAndDays(561600);  
+
+	  
+  }
+}
